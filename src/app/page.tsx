0bb5bc82ab49dff1348047b19c9ace1475131bec
taqui-786 <mdtaqui.jhar@gmail.com>
2024-01-02 00:36:54 +0530

    fixing bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import bg from '../../public/wave2bg.png'
 export default function Home() {
   return (
     <main className="h-full w-full flex flex-col items-center justify-center relative">
-      <Image src={bg} alt="background" fill={true} className='-z-10'/>
+      <Image src={bg} alt="background" fill={true} className='-z-10' unoptimized={true}/>
       <CardForm />
     </main>
   )
