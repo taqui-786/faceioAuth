e6c94b03e95f92b9b3695e87c816d2fcb5162612
taqui-786 <mdtaqui.jhar@gmail.com>
2024-01-10 13:18:29 +0530

    Bug: Blurface fetch request fixed

--- src/components/FaceBlurForm.tsx
+++ src/components/FaceBlurForm.tsx
@@ -39,7 +39,7 @@ const FaceBlurForm: React.FC<Props> = ({}) => {
         const formData = new FormData();
         formData.append("file", imgFile);
 
-        const upload = await fetch("http://localhost:3000/api/blurface", {
+        const upload = await fetch("/api/blurface", {
           method: "POST",
           body: formData,
         });
